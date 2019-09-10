7fe9804072502d20940394c369bbcb2edad3aeca
ihlee <ihlee@crscube.co.kr>
2019-09-10 10:38:57 +0900

    timer

---
+++ test/Timer.spec.js
@@ -0,0 +1,16 @@
+function timerGame(callback) {
+  console.log('Ready....go!');
+  setTimeout(() => {
+    console.log("Time's up -- stop!");
+    callback && callback();
+  }, 1000);
+}
+
+jest.useFakeTimers(); // setTimeout, setInterval, clearTimeout, clearInterval 함수를 mock 함수로 사용 가능하게 함
+
+test('waits 1 second before ending the game', () => {
+  timerGame();
+
+  expect(setTimeout).toHaveBeenCalledTimes(1);
+  expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+});
